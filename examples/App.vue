b8883c51d5ebfd5f40e97226ea2a3d43df55374c
xiaosuom <953001542@qq.com>
2024-07-28 18:20:03 +0800

    init

---
+++ examples/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="wrapper">
+		<HelloWorld msg="You did it!" />
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+header {
+	line-height: 1.5;
+}
+
+.logo {
+	display: block;
+	margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
+
+	.logo {
+		margin: 0 2rem 0 0;
+	}
+
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
+}
+</style>
