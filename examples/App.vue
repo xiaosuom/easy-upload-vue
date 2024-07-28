f7693d7879f6344112ddceff14ad0707506c5c38
xiaosuom <953001542@qq.com>
2024-07-28 18:41:26 +0800

    init

--- examples/App.vue
+++ examples/App.vue
@@ -1,33 +1,12 @@
 <template>
-	<div class="wrapper">
-		<HelloWorld msg="You did it!" />
-	</div>
+	<div class="wrapper">You did it!</div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
+	.wrapper {
 		display: flex;
 		place-items: flex-start;
 		flex-wrap: wrap;
